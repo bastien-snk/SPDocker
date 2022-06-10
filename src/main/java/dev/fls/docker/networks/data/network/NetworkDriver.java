61d6cd88be25dafe037001ec205b5d0b6848b8a2
RootXLS <45496261+rootxls@users.noreply.github.com>
2022-06-10 15:53:48 +0200

    ✨ Add network manager

---
+++ src/main/java/dev/fls/docker/networks/data/network/NetworkDriver.java
@@ -0,0 +1,18 @@
+package dev.fls.docker.networks.data.network;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum NetworkDriver {
+
+    BRIDGE("bridge"),
+    HOST("host"),
+    OVERLAY("overlay"),
+    IPVLAN("ipvlan"),
+    MACVLAN("macvlan"),
+    NONE("none");
+
+    private final String id;
+}
---
+++ src/main/java/dev/fls/docker/networks/data/requests/connect/NetworkConnect.java
@@ -0,0 +1,17 @@
+package dev.fls.docker.networks.data.requests.connect;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class NetworkConnect {
+
+    private final String Container;
+    private EndpointConfig EndpointConfig = new EndpointConfig();
+
+}
